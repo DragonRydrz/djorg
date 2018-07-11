d75f7ba6a0c62bd9debf03bb3f90dbe373be401b
AJ Genung <DragonRydrz@me.com>
2018-07-11 18:44:24 -0400

    another deploy

--- frontend/src/components/Notes.js
+++ frontend/src/components/Notes.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { CardColumns, Card, CardHeader, CardBody, CardText } from 'reactstrap';
 import axios from 'axios';
 
+const host = process.env.HOST || 'http://localhost:8000';
+
 export default class Notes extends Component {
   state = {
     notes: [],
@@ -10,7 +12,7 @@ export default class Notes extends Component {
 
   componentDidMount() {
     axios
-      .get(`https://agenung-djorg.herokuapp.com/api/personal_notes/`, {
+      .get(`${host}/api/personal_notes/`, {
         headers: {
           Authorization: 'Token 4b7652a5e022f474a58faef595c337c5a2788eb1',
         },
@@ -21,7 +23,7 @@ export default class Notes extends Component {
       .catch(err => console.log(err));
 
     axios
-      .get(`https://agenung-djorg.herokuapp.com/api/notes/`, {
+      .get(`${host}/api/notes/`, {
         headers: {
           Authorization: 'Token 4b7652a5e022f474a58faef595c337c5a2788eb1',
         },
