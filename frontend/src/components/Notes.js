4aff780e0f025f073bb6394a6827d2ed7156dd11
AJ Genung <DragonRydrz@me.com>
2018-07-11 19:11:31 -0400

    testing again

--- frontend/src/components/Notes.js
+++ frontend/src/components/Notes.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { CardColumns, Card, CardHeader, CardBody, CardText } from 'reactstrap';
 import axios from 'axios';
 
-const host = 'http://localhost:8000';
+const host = process.env.HOST || 'http://localhost:8000';
 const token = process.env.TOKEN || '4b7652a5e022f474a58faef595c337c5a2788eb1';
 
 export default class Notes extends Component {
